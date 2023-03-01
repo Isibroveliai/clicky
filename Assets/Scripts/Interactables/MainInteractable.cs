aa8fda13abef81d179332276f3dd2dc57f98adfd
zaio <zaio@DESKTOP-E13QL3F>
2023-03-01 22:43:45 +0200

    add score counter and generator button

--- Assets/Scripts/Interactables/MainInteractable.cs
+++ Assets/Scripts/Interactables/MainInteractable.cs
@@ -2,20 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainInteractable : InteractableObject
+public class MainInteractable : MonoBehaviour
 {
-   
     
-    void Start()
-    {
-      
-    }
-
-    //BUG: score registers after clicking outside object, holding mouse button , hovering over and releasing button
-    public override void OnInteract()
-    {
-        base.OnInteract();
-        ClickerManager.Instance.Score++;
-        Debug.Log(System.String.Format("CURRENT SCORE: {0}", ClickerManager.Instance.Score));
-    }
 }
