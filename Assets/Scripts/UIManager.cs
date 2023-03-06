5eba8095d6b0b42f1d01ba1536a4db2d1599b502
Rokas Puzonas <rokas.puz@gmail.com>
2023-03-06 19:31:42 +0200

    Remove GameObject.Find from UIManager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,31 +9,31 @@ using System;
 public class UIManager : MonoBehaviour
 {
     GameManager manager;
-    TMP_Text currencyCounter;
-    Button currencyGenerator;
-    Button upgradeButton;
-    Button optionsButton;
-    GameObject upgradeTab;
-    GameObject optionsTab;
-    GameObject gameOverTab;
-    Button restartButton;
 
     [SerializeField]
+    TMP_Text currencyCounter;
+    [SerializeField]
+    Button currencyGeneratorButton;
+    [SerializeField]
+    Button upgradeButton;
+    [SerializeField]
+    Button optionsButton;
+    [SerializeField]
+    GameObject upgradeTab;
+    [SerializeField]
+    GameObject optionsTab;
+    [SerializeField]
+    GameObject gameOverTab;
+    [SerializeField]
+    Button restartButton;
+
     GameObject currentlyActiveTab;
+
     void Start()
     {
         manager = GameManager.Instance;
 
-        currencyCounter = GameObject.Find("/UI/Counters/CurrencyCounter").GetComponent<TMP_Text>();
-        currencyGenerator = GameObject.Find("UI/CurrencyGenerator/Button").GetComponent<Button>();
-        upgradeTab = GameObject.Find("UI/UpgradesTab");
-        upgradeButton = GameObject.Find("UI/Menubar/Upgrades").GetComponent<Button>();
-        optionsTab = GameObject.Find("UI/OptionsTab");
-        optionsButton = GameObject.Find("UI/Menubar/Options").GetComponent<Button>();
-        gameOverTab = GameObject.Find("UI/GameOverTab");
-        restartButton = GameObject.Find("UI/GameOverTab/Button").GetComponent<Button>();
-
-        currencyGenerator.onClick.AddListener(manager.GenerateCurrency);
+        currencyGeneratorButton.onClick.AddListener(manager.GenerateCurrency);
         //upgradeButton.onClick.AddListener(delegate { ChangeTab(upgradeTab); });
         //optionsButton.onClick.AddListener(delegate { ChangeTab(optionsTab); });
         optionsButton.onClick.AddListener(() => { ChangeTab(optionsTab); });
