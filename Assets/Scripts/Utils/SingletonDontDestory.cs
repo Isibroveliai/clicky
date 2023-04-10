1970b74db3239d8b55828f1fd8109d9e7bc72717
Rokas Puzonas <rokas.puz@gmail.com>
2023-04-10 18:18:57 +0300

    move files to appropriate folders

---
+++ Assets/Scripts/Utils/SingletonDontDestory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public abstract class SingletonDontDestory<T> : MonoBehaviour where T : class
+{
+	public static T instance { get; private set; }
+
+	public void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this as T;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			Destroy(this);
+		}
+
+		Setup();
+	}
+
+	public abstract void Setup();
+}
+
+
+
